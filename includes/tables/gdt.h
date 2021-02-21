f38fd7fa99ad04970ae1748f2e9afaff828433e0
Wafelack <wafelack@protonmail.com>
2021-02-21 11:04:59 +0100

    feat: Simplifying GDT entries with a struct.

--- includes/tables/gdt.h
+++ includes/tables/gdt.h
@@ -3,8 +3,14 @@
 
 #include "../utils/includes.h"
 
+typedef struct __attribute__((__packed__)) {
+    uint32_t limit;
+    uint32_t base;
+    uint8_t type;
+} GDT_ENTRY;
+
 void
-gdt_entry(uint8_t* target, uint32_t limit, uint32_t base, uint8_t type);
+gdt_entry(GDT_ENTRY* entry, uint32_t limit, uint32_t base, uint8_t type);
 
 void
 init_gdt(void);
--- src/gdt.c
+++ src/gdt.c
@@ -6,42 +6,21 @@ extern WRITER writer;
 extern int set_gdt(void);
 
 void
-gdt_entry(uint8_t* target, uint32_t limit, uint32_t base, uint8_t type)
+gdt_entry(GDT_ENTRY* entry, uint32_t limit, uint32_t base, uint8_t type)
 {
 
-    if ((limit > 65536) && ((limit & 0XFFF) != 0xFFF))
-    {
-        PANIC("Limit is too hight or invalid !");
-        return;
-    }
-
-    if (limit > 65536) {
-        // Adjust granularity
-        limit >>= 12;
-        target[6] = 0xC0;
-    } else {
-        target[6] = 0x40;
-    }
-
-    target[0] = limit & 0xFF;
-    target[1] = (limit >> 8) & 0xFF;
-    target[6] = (limit >> 16) & 0xF;
-
-    target[2] = base & 0xFF;
-    target[3] = (base >> 8) & 0xFF;
-    target[4] = (base >> 16) & 0xFF;
-    target[7] = (base >> 24) & 0xFF;
-
-    target[5] = type;
+    entry->limit = limit;
+    entry->base = base;
+    entry->type = type;
 }
 
 void
 init_gdt(void)
 {
-    uint8_t GDT[3][8] = {};
-    gdt_entry(GDT[0], 0, 0, 0);
-    gdt_entry(GDT[1], 0xffffffff, 0, 0x9A);
-    gdt_entry(GDT[2], 0xffffffff, 0, 0x92);
+    GDT_ENTRY GDT[3] = {};
+    gdt_entry(&GDT[0], 0, 0, 0);
+    gdt_entry(&GDT[1], 0xffffffff, 0, 0x9A);
+    gdt_entry(&GDT[2], 0xffffffff, 0, 0x92);
 
     DISABLE_INTERRUPTS();
 
