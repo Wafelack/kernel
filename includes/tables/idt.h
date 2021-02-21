903c098db5dbee318deb49407bdc052022185aba
Wafelack <wafelack@protonmail.com>
2021-02-21 11:12:30 +0100

    feat: Building IDT struct.

---
+++ includes/tables/idt.h
@@ -0,0 +1,14 @@
+#ifndef _IDT_H_
+#define _IDT_H_
+
+#include "utils.h"
+
+typedef struct {
+    uint16_t offset0;
+    uint16_t selector;
+    uint8_t zero;
+    uint8_t type_attr;
+    uint16_t offset1;
+} IDT_ENTRY;
+
+#endif
